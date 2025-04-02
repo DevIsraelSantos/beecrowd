e0cf84449c3af70f9a3162785a2af27147330320
DevIsraelSantos <dev.israelsantos@gmail.com>
2025-04-01 23:22:22 -0300

    1009

---
+++ 1009/1009.js
@@ -0,0 +1,22 @@
+try {
+    var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+} catch {
+    var input = require('fs').readFileSync('./1009/input.txt', 'utf8');
+}
+var lines = input.split('\n');
+
+function get(type) {
+    switch (type) {
+        case 'n': return Number(lines.shift());
+        case 'b': return Boolean(lines.shift() === 'true');
+        default: return lines.shift();
+    }
+}
+
+const func = get()
+const salario = get('n')
+const venda = get('n')
+const commisao = 0.15 * venda
+
+const salarioFinal = salario + commisao
+console.log(`TOTAL = R$ ${salarioFinal.toFixed('2')}`)

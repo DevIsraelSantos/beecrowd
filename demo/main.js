78a23e7a1465351d2745535c2ee525107b1de371
DevIsraelSantos <dev.israelsantos@gmail.com>
2025-07-28 14:37:13 -0300

    normaliza input do main

--- demo/main.js
+++ demo/main.js
@@ -2,6 +2,7 @@ try {
   var input = require("fs").readFileSync("/dev/stdin", "utf8");
 } catch {
   var input = require("fs").readFileSync("./src/main.txt", "utf8");
+  input = input.replace("\r\n", "\n");
 }
 var lines = input.trim().split("\n");
 
